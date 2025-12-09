d95a9f7bd628c7d8a7162606a737250eeb217cc6
zachcantcode1 <zachmillermail@proton.me>
2025-12-08 20:44:26 -0600

    perf: disable prepared statements for postgres client

--- apps/api/db.ts
+++ apps/api/db.ts
@@ -3,7 +3,7 @@ import postgres from "postgres";
 import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL || "postgres://postgres:postgrespassword@localhost:5432/qwikshifts";
-const client = postgres(connectionString);
+const client = postgres(connectionString, { prepare: false });
 const db = drizzle(client, { schema });
 
 export default db;
